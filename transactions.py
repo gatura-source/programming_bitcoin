0cc545f64cae475ffc2cb096a1e7c1a35527ecc6
Your Name <your.email@example.com>
2023-11-14 15:17:08 +0300

    Added TX fee calc

--- transactions.py
+++ transactions.py
@@ -1,10 +1,11 @@
 """
 This module represents the the transactions for Bitcoin
 """
-from helper import SHA_2
-from helper import little_endian_to_int, int_to_little_endian
-from helper import encode_varint, read_varint
-
+from helper import SHA_2 #this one prevents collision
+from helper import little_endian_to_int, int_to_little_endian #conversoin
+from helper import encode_varint, read_varint #as named
+import requests #http requests to shared nodes
+import os #env variables
 class Tx:
     """
     Module represents bitcoin transactions
@@ -73,6 +74,19 @@ class Tx:
             outputs.append(TxOut.parse(stream))
         locktime = little_endian_to_int(stream.read(4))
         return cls(version, inputs, outputs, locktime, testnet=testnet)
+
+    def fee(self):
+        """
+        Calculates the fee using Transaction inputs
+        Tx_inputs - Tx_outputs = fee
+        """
+        fee = 0
+        for _ in self.tx_ins:
+            fee += _.value(testnet=self.testnet)
+        for _ in self.tx_outs:
+            fee -= _.amount
+        return fee
+
 class TxIn:
     """
     A class that implements the inputs of a Bitcoin transaction
@@ -117,6 +131,29 @@ class TxIn:
         sequence = little_endian_to_int(s.read(4))
         return cls(prev_tx, prev_index, script_sig, sequence)
 
+    def fetch_tx(self, testnet=False):
+        """
+        Fetches prev_tx from a TX inputs
+        """
+        return TxFetcher.fetch(self.prev_tx.hex(), testnet=testnet)
+
+    def value(self, testnet=False):
+        """
+        Get the output value by looking up the Tx hash
+        Remember Tx_index contains what output is spent in a transaction
+        Return amount in Satoshi
+        """
+        tx = self.fetch_tx(testnet=testnet)
+        return tx.tx_outs[self.prev_index].amount
+
+    def script_pubkey(self, testnet=False):
+        """
+        Gets ScriptPubKey by looking up Tx Hash
+        Returns a Script Object
+        """
+        tx = self.fetch_tx(testnet=testnet)
+        return tx.tx_outs[self.prev_index].script_pubkey
+
 class TxOut:
     """
     A class that implements the Bitcoins Outputs
@@ -140,5 +177,47 @@ class TxOut:
         return cls(amount, script_pubkey)
 
 
+class TxFetcher:
+    """
+    the class handles looking up the amount in inputs
+    from UTXO set from a full node to calculate transaction
+    fee (inputs - outputs
+    """
+    cache = {}
 
+    @classmethod
+    def get_url(cls, testnet = False):
+        """We are utilizing a shared node
+        hence there is need yo retrieve the whole transaction
+        and verify the ID
+        """
+        if testnet:
+            return f"https://go.getblock.io/{os.environ.get(TESTNET_ACCESS_TOKEN)}"
+        else:
+            return f"https://go.getblock.io/{os.environ.get(MAINNET_ACCESS_TOKEN)}"
+
+    @classmethod
+    def fetch(cls, tx_id, testnet=False, fresh=False):
+        if fresh or (tx_id not in cls.cache):
+            url = f"{cls.get_url(testnet)}/rest/tx/{tx_id}.hex"
+            response = requests.get(url)
+            # the response is in bytes
+            #its more compact an we also know how to
+            #parse a transaction as defined in Tx
+            try:
+                raw = bytes.fromhex(response.text.strip())
+            except ValueError:
+                raise ValueError(f"Unexpected Response: {response.text}")
+            if raw[4] == 0:
+                raw = raw[:4] + raw[6:]
+                tx = TX.parse(BytesIO(raw), testnet=testnet)
+                tx.locktime = little_endian_to_int(raw[-4:])
+            else:
+                tx = Tx.parse(BytesIO(raw), testnet=testnet)
+            if tx.id() != tx_id:
+                raise ValueError(f"Not Same Id: {tx.id()} != {tx_id}")
+
+            cls.cache[tx_id] = tx
+        cls.cache[tx_id].testnet = testnet
+        return cls.cache[tx_id]
 
